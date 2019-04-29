6e0e8c34d05ce07b93f57e894bbd11aa8136cb95
Satoshi Moriai <satoshi.moriai@gmail.com>
2019-04-29 11:07:54 +0900

    Use sizeof() instead of strlen().

--- c/hello.c
+++ c/hello.c
@@ -1,12 +1,11 @@
 /* "Hello, wolrd!" C version. */
 
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main() {
-	const char *buf = "Hello, world!\n";
-	unsigned int len = strlen(buf);
+	const char buf[] = "Hello, world!\n";
+	const unsigned int len = sizeof(buf);
 
 	write(1, buf, len);
 	exit(0);
