a7692623ab7a484a9712d18fd0d305fbc4c07e8d
Satoshi Moriai <satoshi.moriai@gmail.com>
2020-05-05 12:23:03 +0900

    Correct EOL sequences.

--- rust-msdos/src/main.rs
+++ rust-msdos/src/main.rs
@@ -31,7 +31,7 @@ fn exit(status: usize) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let msg = "Hello, world!\n";
+    let msg = "Hello, world!\r\n";
     let buf = msg.as_ptr();
     let len = msg.len();
 
