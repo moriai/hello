d702c580f90b6f8f823852f5516ccf6dc9dec636
Satoshi Moriai <satoshi.moriai@gmail.com>
2020-05-06 20:07:44 +0900

    Print the basename except when the absolute path is specified.

--- size.pl
+++ size.pl
@@ -5,7 +5,7 @@ my $base = basename(getcwd);
 
 while ($_ = $ARGV[0]) {
     ($d,$i,$m,$l,$u,$g,$r,$s) = stat $_;
-    if (/\//) {
+    if (/^\//) {
         printf "%s: %d\n", $_, $s;
     } else {
         printf "%s/%s: %d\n", $base, $_, $s;
