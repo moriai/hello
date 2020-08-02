ffa0acbb61a1b33b6a70babc1f23e6e124d76f17
Satoshi Moriai <satoshi.moriai@gmail.com>
2020-08-02 17:49:44 +0900

    Optimize hello1.rs.

--- rust-msdos/src/bin/hello1.rs
+++ rust-msdos/src/bin/hello1.rs
@@ -8,9 +8,10 @@ use dos::*;
 pub extern "C" fn _start() /*-> !*/ {
     let msg = "Hello, world!\r\n";
     let (result, eflags) = write(1, msg.as_ptr(), msg.len() as u16);
-    if eflags & 0x0001 == 0 {
-        exit(result as u8);
+    let status = if eflags & 0x0001 == 0 {
+        result as u8
     } else {
-        exit(!result as u8);
-    }
+        !result as u8
+    };
+    exit(status);
 }
