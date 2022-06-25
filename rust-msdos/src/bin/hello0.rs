8296884f7b7c88d36ef21b1817abdb2b968625b8
Satoshi Moriai <satoshi.moriai@gmail.com>
2022-06-25 20:46:29 +0900

    Add rust-msdos/src/bin/hello0.rs.

---
+++ rust-msdos/src/bin/hello0.rs
@@ -0,0 +1,57 @@
+#![no_main]
+#![no_std]
+
+use core::arch::asm;
+use core::panic::PanicInfo;
+use core::result::Result;
+
+#[panic_handler]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+fn write(fd: u16, buf: *const u8, len: u16) -> Result<u16, u16> {
+    unsafe {
+        let status: u16;
+        let flags: u16;
+
+        asm!(
+            "mov ah, 0x40",
+            "int 0x21",
+            "pushf",
+            "pop dx",
+            in("dx") buf,
+            in("cx") len,
+            in("bx") fd,
+            out("ax") status,
+            lateout("dx") flags
+        );
+        if flags & 0x01 != 0 {
+            Err(status)
+        } else {
+            Ok(status)
+        }
+    }
+}
+
+pub fn exit(status: u8) -> ! {
+    unsafe {
+        asm!(
+            "mov ah, 0x4c",
+            "int 0x21",
+            in("al") status
+        );
+    }
+    loop {}
+}
+
+#[link_section=".startup"]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let msg = "Hello, world!\r\n";
+    let status = match write(1, msg.as_ptr(), msg.len() as u16) {
+        Ok(_) => 0,
+        Err(err) => err as u8
+    };
+    exit(status);
+}
